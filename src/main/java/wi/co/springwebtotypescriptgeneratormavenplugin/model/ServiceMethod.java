295071bbcb468b559c0f41d7d5cd54eee49d90e0
Cornelius Wichering <cornelius.lilge@gmail.com>
2017-01-21 00:43:38 +0100

    Added basic return type parsing

--- src/main/java/wi/co/springwebtotypescriptgeneratormavenplugin/model/ServiceMethod.java
+++ src/main/java/wi/co/springwebtotypescriptgeneratormavenplugin/model/ServiceMethod.java
@@ -13,11 +13,13 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class ServiceMethod {
 
+	public final String resultType;
 	public final String name;
 	public final RequestMappingAnnotation requestMappingAnnotation;
 	public final List<RequestParameter> parameters;
 
-	private ServiceMethod(final String name, final RequestMappingAnnotation requestMappingAnnotation, final List<RequestParameter> parameters) {
+	private ServiceMethod(final String resultType, final String name, final RequestMappingAnnotation requestMappingAnnotation, final List<RequestParameter> parameters) {
+		this.resultType = resultType;
 		this.name = name;
 		this.requestMappingAnnotation = requestMappingAnnotation;
 		this.parameters = parameters;
@@ -28,10 +30,11 @@ public class ServiceMethod {
 	}
 
 	public static ServiceMethod parse(final MethodDeclaration method) {
+		final String resultType = method.getType().toString();
 		final String name = method.getNameAsString();
 		final RequestMappingAnnotation requestMappingAnnotation = RequestMappingAnnotation.parse(method).get();
 		final List<RequestParameter> parameters = parseParameters(method);
-		return new ServiceMethod(name, requestMappingAnnotation, parameters);
+		return new ServiceMethod(resultType, name, requestMappingAnnotation, parameters);
 	}
 
 	private static List<RequestParameter> parseParameters(final MethodDeclaration method) {
